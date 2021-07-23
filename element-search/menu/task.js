47527853f24f51b25d3c28c81c1856e2a741203c
Maksim Astafev <maksim.astafev@gmail.com>
2021-07-23 18:07:56 +0300

    Сделал полноценную проверку кликов по открытым меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,17 +3,30 @@ const menuActiveClass = " menu_active";
 let originalMenuClass = "";
 let activeMenu = null;
 
+function menuClick(clickItem) {
+  // Нет открытого меню.    Открыть меню.
+  if(activeMenu === null) {
+    activeMenu = clickItem;
+    activeMenu.className += menuActiveClass;
+  } else {
+    // Есть открытое меню, кликнуто по открытому меню.    Закрыть открытое меню.
+    if(clickItem === activeMenu) {
+      activeMenu.className = originalMenuClass;
+      activeMenu = null;
+    } else {
+      // Есть открытое меню, кликнуто по закрытому меню.    Закрыть открытое меню. Открыть кликнутое меню.
+      activeMenu.className = originalMenuClass;
+      activeMenu = clickItem;
+      activeMenu.className += menuActiveClass;
+    }
+  }
+  return false;
+}
+
 for(let i=0; i<menuLinks.length; i++) {
   let menuItem = menuLinks.item(i).closest(".menu__item").querySelector(".menu_sub");
   if(menuItem !== null) {
-    menuLinks.item(i).onclick = function () {
-      if(activeMenu !== null) {
-        activeMenu.className = originalMenuClass;
-      }
-      originalMenuClass = menuItem.className;
-      menuItem.className += menuActiveClass;
-      activeMenu = menuItem;
-      return false;
-    }
+    originalMenuClass = menuItem.className;
+    menuLinks.item(i).onclick = () => menuClick(menuItem);
   }
 }
