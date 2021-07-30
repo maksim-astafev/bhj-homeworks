e188db7b57bb7021ac14d5f9337cfbb2ac2288b9
Maksim Astafev <maksim.astafev@gmail.com>
2021-07-30 23:19:18 +0300

    выполнена декомпозиция и добавлена дуракоустойчивость

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,6 +1,7 @@
 const fontSizeClass = "font-size";
 const fontSizeActiveClass = "font-size_active";
 const  fontSizeBigData = "big";
+const  fontSizeNormalData = undefined;
 const  fontSizeSmallData = "small";
 const bookNormalClass = "book";
 const bookBigClass = bookNormalClass + " book_fs-big";
@@ -10,38 +11,58 @@ let currentSizeButton = null;
 const fontSizeButtons = Array.from(document.querySelectorAll(`.${fontSizeClass}`));
 const bookElement = document.querySelector(`.${bookNormalClass}`);
 
+function switchSizeButton(clickedButton) {
+  currentSizeButton.classList.remove(fontSizeActiveClass);
+  currentSizeButton = clickedButton;
+  currentSizeButton.classList.add(fontSizeActiveClass);
+}
+
+function switchSizeInBook(currentSizeButton) {
+  switch(currentSizeButton.dataset.size) {
+    case fontSizeSmallData:
+      bookElement.className = bookSmallClass;
+      break;
+    case fontSizeBigData:
+      bookElement.className = bookBigClass;
+      break;
+    default:
+      bookElement.className = bookNormalClass;
+      break
+  }
+}
+
+function setDefaultButton(defaultFontSizeData) {
+  if(currentSizeButton === null) {
+    fontSizeButtons.forEach((toActivate) => {
+      if(toActivate.dataset.size === defaultFontSizeData) {
+        toActivate.classList.add(fontSizeActiveClass);
+        currentSizeButton = toActivate;
+        switchSizeInBook(currentSizeButton);
+      }
+    });
+  }
+}
+
 fontSizeButtons.forEach((sizeButton) => {
+  // защита, если класс задан нескольким кнопкам
   if(sizeButton.classList.contains(fontSizeActiveClass)) {
-    currentSizeButton = sizeButton;
+    if(currentSizeButton === null) {
+      currentSizeButton = sizeButton;
+      switchSizeInBook(currentSizeButton);
+    } else {
+      sizeButton.classList.remove(fontSizeActiveClass);
+    }
   }
 
   sizeButton.onclick = (event) => {
     if(event.target !== currentSizeButton) {
-      currentSizeButton.classList.remove(fontSizeActiveClass);
-      currentSizeButton = event.target;
-      currentSizeButton.classList.add(fontSizeActiveClass);
-
-      switch(sizeButton.dataset.size) {
-        case fontSizeSmallData:
-          bookElement.className = bookSmallClass;
-          break;
-        case fontSizeBigData:
-          bookElement.className = bookBigClass;
-          break;
-        default:
-          bookElement.className = bookNormalClass;
-          break
-      }
+      switchSizeButton(event.target);
+      switchSizeInBook(currentSizeButton);
     }
 
     return false;
   }
 });
 
-if(currentSizeButton === null) {
-  fontSizeButtons.forEach((toActivate) => {
-    if(toActivate.dataset.size === fontSizeBigData) {
-      toActivate.classList.add(fontSizeActiveClass);
-    }
-  });
-}
+// защита, если класс не задан ни одной кнопке
+setDefaultButton(fontSizeNormalData);
