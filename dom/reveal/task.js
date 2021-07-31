a8ef26c225a4b5065cfb3e4d9cc0a9266875e39e
Maksim Astafev <maksim.astafev@gmail.com>
2021-07-31 09:56:15 +0300

    рефакторинг по рекомендациям преподавателя

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -4,9 +4,8 @@ const revealElements = Array.from(document.querySelectorAll(`.${revealClass}`));
 
 const isInViewport = function(element) {
   const viewportHeight = window.innerHeight;
-  const elementTop = element.getBoundingClientRect().top;
-  const elementBottom = element.getBoundingClientRect().bottom;
-  return (elementTop < viewportHeight && elementBottom > 0) ? true : false;
+  const { top, bottom } = element.getBoundingClientRect();
+  return top < viewportHeight && bottom > 0;
 }
 
 document.onscroll = (event) => {
